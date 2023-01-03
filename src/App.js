39ff215f8d1d8b218d9edded60b46838417e977f
Erika Sha <erikaasha05@gmail.com>
2023-01-03 18:48:24 -0500

    created NewBoardForm component and applied styling to empty input values

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from "./logo.svg";
 import "./App.css";
+import NewBoardForm from "./components/NewBoardForm";
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
         >
           Learn React
         </a>
+        <NewBoardForm />
       </header>
     </div>
   );
---
+++ src/components/NewBoardForm.js
@@ -0,0 +1,71 @@
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import "./NewBoardForm.css";
+
+const kDefaultFormData = {
+  title: "",
+  owner: "",
+};
+
+const NewBoardForm = (props) => {
+  const [formData, setFormData] = useState(kDefaultFormData);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    props.handleBoardSubmit(formData);
+    setFormData(kDefaultFormData);
+  };
+
+  const handleFormData = (event) => {
+    const dataValue = event.target.value;
+    const dataField = event.target.name;
+
+    const newData = { ...formData, [dataField]: dataValue };
+    setFormData(newData);
+  };
+
+  const ifTitleEmpty = formData.title ? "" : "empty";
+  const ifOwnerEmpty = formData.owner ? "" : "empty";
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="title">Title</label>
+        <input
+          className={ifTitleEmpty}
+          type="text"
+          id="title"
+          name="title"
+          value={formData.title}
+          onChange={handleFormData}
+        />
+      </div>
+      <div>
+        <label htmlFor="owner">Owner's Name</label>
+        <input
+          className={ifOwnerEmpty}
+          type="text"
+          id="owner"
+          name="owner"
+          value={formData.owner}
+          onChange={handleFormData}
+        />
+      </div>
+      <div>
+        <p>
+          Preview: {formData.title} - {formData.owner}
+        </p>
+      </div>
+      <div>
+        <input type="submit" value="Add Board"></input>
+      </div>
+      <button>Hide New Board Form</button>
+    </form>
+  );
+};
+
+NewBoardForm.propTypes = {
+  handleBoardSubmit: PropTypes.func,
+};
+
+export default NewBoardForm;
