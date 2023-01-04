e13b245e39ec26aaf078c5557e493982af606580
Erika Torres Figueroa <torrespurple@gmail.com>
2023-01-04 13:44:09 -0500

    added state, completed newCardForm

---
+++ src/components/NewCardForm.js
@@ -0,0 +1,50 @@
+import React, {useState} from "react";
+
+const kDefaultFormState = {
+  message: ""
+};
+
+const NewCardForm = ({handleCardSubmit}) => {
+  
+  const [cardData, setCardData] = useState(kDefaultFormState);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    handleCardSubmit(cardData);
+    setCardData(kDefaultFormState);
+  };
+  
+  const handleCardFormData = (event) => {
+    const dataValue = event.target.value;
+    const dataName = event.target.name;
+
+    const newCardData = {...cardData, [dataName]: dataValue}
+    setCardData(newCardData);
+  };
+
+  const invalidInput = cardData.message ? "" : "empty"
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="message">Message</label>
+        <input
+          className={invalidInput}
+          type="text" 
+          id="message" 
+          name="message" 
+          value={cardData.message} 
+          onChange={handleCardFormData} 
+        />
+        <p>
+          Preview: {cardData.message}
+        </p>
+      </div>
+      <div>
+        <input type="submit" value="Add a Card" />
+      </div>
+    </form>
+  )
+};
+
+export default NewCardForm;
